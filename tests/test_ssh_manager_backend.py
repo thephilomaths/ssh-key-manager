9651c275b733321df84ccc18346eebd596798663
Kartik Behl <kartikbehl99@gmail.com>
2020-08-27 23:06:16 +0530

    Add empty database function

--- tests/test_ssh_manager_backend.py
+++ tests/test_ssh_manager_backend.py
@@ -1,5 +1,20 @@
 from ssh_manager_backend import __version__
+from ssh_manager_backend.db.database import db_session
+from ssh_manager_backend.db.schema import AccessControl, Key, KeyMapping, User
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def empty_db():
+    session = db_session()
+    session.query(User).delete()
+    session.query(Key).delete()
+    session.query(KeyMapping).delete()
+    session.query(AccessControl).delete()
+    session.commit()
+    db_session.remove()
+
+
+empty_db()
